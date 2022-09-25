6a075f898e773fb69252ff41f3ab655636598aad
MaTa31 <mickael-fernandez@live.fr>
2022-09-25 20:19:41 +0200

    v1.3 Ajout des derniers éléments de la premiere page du site. Ajout de nouvelles des régles responsive (manques encore 2/3 éléments pour 100%)

---
+++ SeaCleaner/script.js
@@ -0,0 +1,24 @@
+var  nav = document.getElementById('back');
+
+
+      
+      window.onscroll = function(){
+
+      	if (window.pageYOffset >0) {
+
+            back.style.filter = "drop-shadow(0 0 0.75rem #37D1F3)";   
+            back.style.display = "flex";
+            back.style.justifyContent = "center";
+            back.style.alignItems = "center";
+            back.style.position =  "fixed";
+            back.style.right = "20px";
+            back.style.bottom = "20px";
+            back.style.zIndex = "10";
+            
+      	}
+      	else{
+            
+            back.style.display = "none";
+        
+        }
+      }
