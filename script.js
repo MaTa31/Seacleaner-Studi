44e521f158ddc5276fe5fb9b2e2e80df1bc76f4d
MaTa31 <mickael-fernandez@live.fr>
2022-10-02 12:11:45 +0200

    V2.0 - Correction disposition footer - ajout burgeur menu vue mobile

--- script.js
+++ script.js
@@ -21,3 +21,8 @@ var  nav = document.getElementById('back');
         }
       }
 
+function menuOnClick() {
+      document.getElementById("menu-bar").classList.toggle("change");
+      document.getElementById("nav").classList.toggle("change");
+      document.getElementById("menu-bg").classList.toggle("change-bg");
+      }
